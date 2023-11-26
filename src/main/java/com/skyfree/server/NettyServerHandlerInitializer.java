60d188a2c4de365c99aef2563aa55e2826c9a571
to_tree <458984307@qq.com>
2023-11-26 16:16:58 +0800

    添加空闲检测(广播模式)

--- src/main/java/com/skyfree/server/NettyServerHandlerInitializer.java
+++ src/main/java/com/skyfree/server/NettyServerHandlerInitializer.java
@@ -1,16 +1,13 @@
 package com.skyfree.server;
 
 import com.skyfree.server.handler.NettyServerHandler;
-import com.skyfree.server.handler.ServerIdleStateHandler;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.protobuf.ProtobufDecoder;
-import io.netty.handler.codec.protobuf.ProtobufEncoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
 
 public class NettyServerHandlerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
@@ -21,7 +18,7 @@ public class NettyServerHandlerInitializer extends ChannelInitializer<SocketChan
 //                转码通道处理
                 .addLast("encode",new StringEncoder())
 
-                .addLast(new ServerIdleStateHandler())
+                .addLast(new IdleStateHandler(5,5,10, TimeUnit.SECONDS))
 //                聊天服务通道处理
 //                自定义的处理器如果客户端有前缀字段，在服务端必须要添加相同的字段，才能对应处理
                 .addLast("chat",new NettyServerHandler());
--- src/main/java/com/skyfree/server/handler/NettyServerHandler.java
+++ src/main/java/com/skyfree/server/handler/NettyServerHandler.java
@@ -1,13 +1,14 @@
 package com.skyfree.server.handler;
 
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.util.concurrent.GlobalEventExecutor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @ChannelHandler.Sharable
@@ -19,6 +20,7 @@ public class NettyServerHandler extends SimpleChannelInboundHandler<String> {
 
     private static final ChannelGroup group = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
+    private static final ConcurrentHashMap<Channel,Long> concurrentHashMap = new ConcurrentHashMap<>();
     /**
      * 读取消息通通道
      */
@@ -112,5 +114,51 @@ public class NettyServerHandler extends SimpleChannelInboundHandler<String> {
        Channel channel = ctx.channel();
         System.out.println("[" + channel.remoteAddress() + "] leave the room");
         ctx.close().sync();
+    }
+
+    /**
+     * 如果链接闲置时间过长，会触发IdleStateEvent事件，在ChannelInboundHandler中可以覆盖userEventrighered方法处理IdleStateEvent
+     * @param ctx
+     * @param evt
+     * @throws Exception
+     */
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+//         判断事件是否为IdleStateEvent
+        if(evt instanceof IdleStateEvent) {
+            IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
+            IdleState state = idleStateEvent.state();
+            String evtState = null;
+            Channel channel = ctx.channel();
+            Long count = concurrentHashMap.getOrDefault(channel,0L);
+            switch (state)
+            {
+                    case READER_IDLE:
+                    evtState = "读空闲";
+                    break;
+                    case WRITER_IDLE:
+                    evtState = "写空闲";
+                    break;
+                    case ALL_IDLE:
+                        evtState = "读写空闲";
+                        count ++;
+                    break;
+                default:
+                    break;
+            }
+            log.info("userServerEventTriggered-evtState:{}",evtState);
+//            空闲计数达5次，进行测试链接
+            if(count > 2L)
+            {
+                ctx.writeAndFlush("测试客户端是佛能接受信息")
+//                        发送失败时关闭通道，再或者可以达到空间多少次后，进行关闭通道
+                        .addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+                concurrentHashMap.remove(channel);
+            }
+            concurrentHashMap.put(channel,count);
+        } else {
+//           事件不是一个IdleStateEvent,就将它传递给下一个处理程序
+            super.userEventTriggered(ctx,evt);
+        }
     }
 }
